4549464962e0904022836f418eb5955779e05e4a
Alexander Grein <alexander.grein@gmail.com>
2024-03-21 17:59:16 +0100

    Add missing comma

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
         'depends' => [
             'typo3'  => '9.5.0-12.4.99',
         ],
-        'conflicts' => []
+        'conflicts' => [],
         'suggests' => [],
     ],
 ];
